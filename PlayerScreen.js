18ad1d40d5765c7ff854da20c0d68356171e3aeb
Emmet <emmetdoyle@ymail.com>
2017-11-26 21:34:06 +0000

    Initial Commit

---
+++ PlayerScreen.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {
+	StyleSheet,
+	Text,
+	View
+} from 'react-native';
+
+export class PlayerScreen extends Component
+{
+	static navigationOptions = {
+		title: 'Player',
+	};
+
+	constructor()
+	{
+		super();
+	}
+	
+
+
+	render()
+	{
+		Spotify.playURI("spotify:track:7xyX8M8Pklyq9jFcYlaYpU", 0, 52, (error) => {
+      		if(error){
+          		console.log(error);
+      		}
+    	});
+
+		return (
+			<View style={styles.container}>
+				<Text style={styles.greeting}>
+					This is the player screen
+				</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	greeting: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+});
